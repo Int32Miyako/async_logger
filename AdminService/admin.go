c342581a32066e1b6074bb44dd7fb0b687b98a11
Bogdan <bogdan47.2006@gmail.com>
2025-10-31 22:22:30 +0300

    минор

--- AdminService/admin.go
+++ AdminService/admin.go
@@ -1,9 +1,38 @@
 package AdminService
 
 import (
+	"async_logger/AdminService/logging"
+	"async_logger/AdminService/statistics"
 	pb "async_logger/codegen"
+
+	"google.golang.org/grpc"
 )
 
-type BaseServer struct {
+// уже делаем ручки
+// на вход должны принять сгенерированный объект запроса
+// кроме контекста
+
+type ServerAPI struct {
 	pb.UnimplementedAdminServer
+}
+
+func RegisterServerAPI(gRPC *grpc.Server) {
+	pb.RegisterAdminServer(gRPC, &ServerAPI{})
+}
+
+func (s *ServerAPI) Statistics(
+	interval *pb.StatInterval,
+	server pb.Admin_StatisticsServer,
+) error {
+	statistics.GetStatistics()
+	return nil
+}
+
+func (s *ServerAPI) Logging(
+	nothing *pb.Nothing,
+	server pb.Admin_LoggingServer,
+
+) error {
+	logging.GetLogs()
+	return nil
 }
